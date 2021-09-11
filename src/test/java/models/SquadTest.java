8b961a5d50151ee0e503d49a366d5aa469ba0330
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-11 09:31:16 +0300

    Create test to validate Squad object instantiates correctly.

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,10 +2,17 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SquadTest {
+
+    @Test
+    public void newSquad_instantiatesCorrectly() {
+        Squad squad = new Squad("Avengers", "Save the Universe", 14);
+        assertTrue(squad instanceof Squad);
+    }
 
 
     @Before
