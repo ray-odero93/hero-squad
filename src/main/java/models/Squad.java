9ab98c8e0cbfa19e9f16933301056d2324e48052
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-11 09:41:32 +0300

    Create method to return name of Squad object.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,7 +1,17 @@
 package models;
 
 public class Squad {
-    private String squadName;
-    private String dedicatedCause;
+    private String name;
+    private String mission;
+    private int participants;
 
+    public Squad( String name, String mission, int participants){
+        this.name = name;
+        this.mission = mission;
+        this.participants = participants;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
