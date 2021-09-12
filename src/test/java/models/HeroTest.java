b19e39bd1fccfe2bd0cddf80f8bc46977b53093f
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-12 17:31:30 +0300

    Create test for validating creation of multiple instances of Hero object.

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -15,27 +15,35 @@ public class HeroTest {
     }
 
     @Test
-    public void newHero_instantiatesWithCorrectName_true() throws Exception{
+    public void newHero_instantiatesWithCorrectName_string() throws Exception{
         Hero hero = new Hero("ray", 28, "intellectual-animist","truth");
         assertEquals("", hero.getName());
     }
 
     @Test
-    public void newHero_instantiatesWithCorrectAge_true() throws Exception{
+    public void newHero_instantiatesWithCorrectAge_true_integer() throws Exception{
         Hero hero = new Hero("ray", 28, "intellectual-animist","truth");
         assertEquals(28, hero.getAge());
     }
 
     @Test
-    public void newHero_instantiatesWithCorrectPower() throws Exception{
+    public void newHero_instantiatesWithCorrectPower_string() throws Exception{
         Hero hero = new Hero("ray", 28, "intellectual-animist","truth");
         assertEquals("intellectual-animist", hero.getPower());
     }
 
     @Test
-    public void newHero_instantiatesWithCorrectWeakness() throws Exception{
+    public void newHero_instantiatesWithCorrectWeakness_string() throws Exception{
         Hero hero = new Hero("ray", 28, "intellectual-animist","cake");
         assertEquals("cake", hero.getWeakness());
+    }
+
+    @Test
+    public void heroArray_getAllInstances_true() {
+        Hero hero = new Hero("ray", 28, "intellectual-animist","cake");
+        Hero otherHero = new Hero("purity", 25, "total killer-babe","diamonds");
+        assertTrue(Hero.getInstances().contains(hero));
+        assertTrue(Hero.getInstances().contains(otherHero));
     }
 
     @Before
