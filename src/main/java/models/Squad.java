d70667cd710b2a33f945ecbc51e115c4b4f62e42
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-12 22:24:44 +0300

    Create methods to return squad list and members list.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,9 +1,17 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Dictionary;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Squad {
     private String name;
     private String mission;
     private int participants;
+    private static ArrayList<Squad> instances = new ArrayList<>();
+    private ArrayList<Hero> members = new ArrayList<>();
 
     public Squad( String name, String mission, int participants){
         this.name = name;
@@ -23,4 +31,24 @@ public class Squad {
     public int getLimit() {
         return participants;
     }
+
+    public static ArrayList<Squad> getInstances() {
+        return instances;
+    }
+
+    public void addSquadMembers(Hero hero) {
+        members.add(hero);
+    }
+
+    public ArrayList<Hero> getSquadMembers() {
+        return members;
+    }
+
+//    public static void clearSquadsList() {
+//        instances.clear();
+//    }
+
+//    public void clearMembersList() {
+//        getSquadMembers().clear();
+//    }
 }
